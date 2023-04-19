207bcd0ff7a373997efb6f37b93fef3d47b6bd09
AKK51580 <131166269+AKK51580@users.noreply.github.com>
2023-04-19 15:08:08 +0300

    2

---
+++ datan-lukeminen-objekteista.js
@@ -0,0 +1,82 @@
+const games = [ {
+    name: "Half-Life Alyx",
+    developer: "Valve",
+    genre: "Action, FPS",
+    releaseYear: "2020",
+    URL: "https://www.half-life.com/en/alyx/",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/-KSnQ94aEBKFoPUxqnWLoupQdIo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19821673/COMBINE_ELEVATOR_4K.jpg",
+    score: 96,
+  },
+{
+    name: "Super Hot VR",
+    developer: "SUPERHOT Team",
+    genre: "Action, FPS",
+    releaseYear: "2016",
+    URL: "https://superhotgame.com/",
+    image:
+      "https://cdn.akamai.steamstatic.com/steam/apps/690040/ss_2aa74cc135fcc68fca7d18cbcf5233a73fd6fa25.1920x1080.jpg?t=1667473713",
+    score: 93,
+  },
+{
+    name: "Zero Caliber",
+    developer: "XREAL Games",
+    genre: "Action, FPS",
+    releaseYear: "2018",
+    URL: "https://www.xrealgames.com/zerocalibervr/",
+    image:
+      "https://cdn.akamai.steamstatic.com/steam/apps/877200/ss_26d8149fa566f14ff9c7e06f0bf4be734d3586e2.1920x1080.jpg?t=1679322301",
+    score: 95,
+  } ];
+
+  
+const gameListElement = document.querySelector("#container");
+
+for (let i = 0; i < games.length; i++) {
+  const gameName = games[i].name;
+  const gameDeveloper = "Developer: " + games[i].developer;
+  const gameGenre = games[i].genre;
+  const gameReleaseYear = games[i].releaseYear;
+  const gameURL = games[i].URL;
+  const gameImage = games[i].image;
+  const gameScore = games[i].score;
+
+  const onegameElements = document.createElement("div");
+  onegameElements.classList.add("onegame");
+
+  const gameNameElement = document.createElement("h2");
+  gameNameElement.textContent = gameName;
+  onegameElements.appendChild(gameNameElement);
+
+  const gameDeveloperElement = document.createElement("p");
+  gameDeveloperElement.textContent = gameDeveloper;
+  onegameElements.appendChild(gameDeveloperElement);
+  
+  const gameGenreElement = document.createElement("p");
+  gameGenreElement.textContent = gameGenre;
+  onegameElements.appendChild(gameGenreElement);
+  
+  const gameReleaseYearElement = document.createElement("p");
+  gameReleaseYearElement.textContent = gameReleaseYear;
+  onegameElements.appendChild(gameReleaseYearElement);
+  
+  const gameURLElement = document.createElement("p");
+  gameURLElement.textContent = gameURL;
+  onegameElements.appendChild(gameURLElement);
+  
+  const gameImageElement = document.createElement("img");
+  gameImageElement.src = gameImage;
+  onegameElements.appendChild(gameImageElement);
+  
+const gameElement = document.createElement("p");
+gameElement.textContent = "Score";
+  gameElement.classList.add("text-score");
+onegameElements.appendChild(gameElement);
+
+  const gameScoreElement = document.createElement("p");
+  gameScoreElement.textContent = gameScore;
+  gameScoreElement.classList.add("score");
+  onegameElements.appendChild(gameScoreElement);
+
+  gameListElement.appendChild(onegameElements);
+}
